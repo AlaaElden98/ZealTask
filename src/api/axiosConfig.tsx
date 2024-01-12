af9c208418999ec571da28be2e3e0ef787215bb5
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2024-01-12 21:19:58 +0200

    seperate update axiosToken from axiosConfig

--- src/api/axiosConfig.tsx
+++ src/api/axiosConfig.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import Config from 'react-native-config';
 
-import {getToken} from '../helpers/asyncStorageHelpers';
-
-export const setAxiosConfigurations = async () => {
+export const setAxiosConfigurations = () => {
   axios.defaults.baseURL = Config.API_BASE_URL;
   axios.defaults.headers.common['Content-Type'] = 'application/json';
-  axios.defaults.headers.common.token = await getToken();
+};
+
+export const setAxiosToken = (token: string | null) => {
+  axios.defaults.headers.common.token = typeof token === 'string' ? token : '';
 };
