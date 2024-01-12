1739a3f2800a8687acb7180cc001f8481994eead
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2024-01-12 07:20:55 +0200

    [apis] update auth apis and interface

--- src/api/authApis.tsx
+++ src/api/authApis.tsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-import {ApiResponse} from '../interfaces/ApiResponsesInterfaces';
+import {AuthApiResponse} from '../interfaces/ApiResponsesInterfaces';
 
 export const registerUser = async (
   email: string,
   password: string,
   name: string,
 ) => {
-  const result: ApiResponse = {success: false, errorText: ''};
+  const result: AuthApiResponse = {success: false, errorText: ''};
   try {
-    await axios.post('/register', {email, password, name});
+    const response = await axios.post('/register', {email, password, name});
     result.success = true;
+    result.userData = response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       result.errorText = error.response?.data;
@@ -20,10 +21,11 @@ export const registerUser = async (
 };
 
 export const logUser = async (email: string, password: string) => {
-  const result: ApiResponse = {success: false, errorText: ''};
+  const result: AuthApiResponse = {success: false, errorText: ''};
   try {
-    await axios.post('/login', {email, password});
+    const response = await axios.post('/login', {email, password});
     result.success = true;
+    result.userData = response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       result.errorText = error.response?.data.error;
--- src/interfaces/ApiResponsesInterfaces.tsx
+++ src/interfaces/ApiResponsesInterfaces.tsx
@@ -1,4 +1,13 @@
-export interface ApiResponse {
+export interface AuthApiResponse {
   success: boolean;
   errorText: string;
+  userData?: UserData;
+}
+
+interface UserData {
+  admin: {
+    email: string;
+    name: string;
+  };
+  token: string;
 }
