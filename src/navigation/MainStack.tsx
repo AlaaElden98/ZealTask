32586ba738cd1920e4feb60eb895ff4b55f09953
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2024-01-13 07:39:56 +0200

    wrap MainStack with react query provider

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {HomeScreen} from '../screens';
 import {SCREEN_NAMES} from '../constant/navigationConstants';
 
 const MainStack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export const MainStackScreens = () => (
-  <MainStack.Navigator>
-    <MainStack.Screen name={SCREEN_NAMES.HomeScreen} component={HomeScreen} />
-  </MainStack.Navigator>
+  <QueryClientProvider client={queryClient}>
+    <MainStack.Navigator>
+      <MainStack.Screen name={SCREEN_NAMES.HomeScreen} component={HomeScreen} />
+    </MainStack.Navigator>
+  </QueryClientProvider>
 );
